e299edf24a642a524b4b5e739dadc55d8a47fcba
florence martino <florence.martino@lifen.fr>
2022-05-27 00:42:39 +0200

    Click on movie add in wishlist

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,10 +1,12 @@
 class MoviesController < ApplicationController
+  skip_before_action :verify_authenticity_token
 
   def new
     @movie = Movie.new
   end
 
   def create
+
     movie = Movie.new(movie_params)
     movie.save!
 
--- app/javascript/controllers/search_controller.js
+++ app/javascript/controllers/search_controller.js
@@ -4,21 +4,53 @@ export default class extends Controller {
   static targets = [ "title" ]
 
   movie(event) {
-    console.log(`You search for: ${this.title}`)
-
     const results = document.querySelector("#results")
 
     results.innerHTML = ""
 
     fetch(`https://imdb-api.com/en/API/SearchTitle/${event.params.key}/${this.title}`)
-      .then(response => response.json())
-      .then((movieResults) => {
-        movieResults.results.forEach((movieResult) => {
-          const movieTag = `<li class="list-inline-item"><img src="${movieResult.image}" alt="" width="100%" height="100%"><p>${movieResult.title}</p></li>`
-          results.insertAdjacentHTML("beforeend", movieTag)
-        })
+    .then(response => response.json())
+    .then((movieResults) => {
+      movieResults.results.forEach((movieResult) => {
+
+        const movieTitle = movieResult.title
+        const movieTag = `
+        <li class="list-inline-item">
+            <img id='imageMovie' src="${movieResult.image}" alt="" width="100%" height="100%" title="${movieTitle}"
+              <p>${movieTitle}</p>
+        </li>`
+        results.insertAdjacentHTML("beforeend", movieTag)
+
+
       })
+
+      let movieTitle;
+      const selectedMovie = document.getElementById("imageMovie");
+
+      selectedMovie.addEventListener("click", (event) => {
+        console.log( event.currentTarget.title)
+        movieTitle = event.currentTarget.title
+
+        fetch('/movies', {
+          method: 'POST',
+          headers:
+            {
+              "Content-Type": "application/json",
+              "Accept": "text/html"
+            },
+          body: JSON.stringify({title: movieTitle})
+        })
+        .then(response => response.json())
+        .then((data) => {
+          console.log(data);
+        });
+      });
+
+    })
+
+
   }
+
 
   get title() {
     return this.titleTarget.value

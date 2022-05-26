16a145b27335c69fa39e0c8229378d9ed8132619
florence martino <florence.martino@lifen.fr>
2022-05-26 12:22:03 +0200

    stimulus: search movie

--- app/javascript/controllers/search_controller.js
+++ app/javascript/controllers/search_controller.js
@@ -4,8 +4,23 @@ export default class extends Controller {
   static targets = [ "title" ]
 
   movie() {
-    const element = this.titleTarget
-    const name = element.value
-    console.log(`Hello, ${name}!`)
+    console.log(`You search for: ${this.title}`)
+
+    const results = document.querySelector("#results")
+
+    results.innerHTML = ""
+
+    fetch(`https://imdb-api.com/en/API/SearchTitle/k_rvvt7xsh/${this.title}`)
+      .then(response => response.json())
+      .then((movieResults) => {
+        movieResults.results.forEach((movieResult) => {
+          const movieTag = `<li class="list-inline-item"><img src="${movieResult.image}" alt="" width="100%" height="100%"><p>${movieResult.title}</p></li>`
+          results.insertAdjacentHTML("beforeend", movieTag)
+        })
+      })
+  }
+
+  get title() {
+    return this.titleTarget.value
   }
 }
